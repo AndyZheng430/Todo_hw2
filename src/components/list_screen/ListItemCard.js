8f50bf691f2f25d7e47ceeeb2beee2d3ab400461
AndyZheng430 <zhenga6@bxscience.edu>
2019-10-14 20:03:17 -0400

    Adding styling to each list screen and buttons to each list item.

--- src/components/list_screen/ListItemCard.js
+++ src/components/list_screen/ListItemCard.js
@@ -8,13 +8,22 @@ export class ListItemCard extends Component {
                     {this.props.listItem.description}
                 </div>
                 <div className='list_item_card_assigned_to'>
-                    Assigned To: <strong>{this.props.listItem.assignedTo}</strong>
+                    Assigned To: <strong>{this.props.listItem.assigned_to}</strong>
                 </div>
                 <div className='list_item_card_due_date'>
-                    {this.props.listItem.dueDate}
+                    {this.props.listItem.due_date}
                 </div>
                 <div className='list_item_card_completed'>
-                    {this.props.listItem.completed}
+                    {this.props.listItem.completed==true ? 'Completed': 'Pending'}
+                </div>
+                <div className='list_item_up_arrow'>
+                    &#8593;
+                </div>
+                <div className='list_item_down_arrow'>
+                    &#8595;
+                </div>
+                <div className='list_item_delete'>
+                    &#x2715;
                 </div>
             </div>
         )
--- src/components/list_screen/ListItemsTable.js
+++ src/components/list_screen/ListItemsTable.js
@@ -5,10 +5,13 @@ export class ListItemsTable extends Component {
     render() {
         return (
             <div id="list_items_container">
-                <div className="list_item_task_header">Task</div>
-                <div className="list_item_due_date_header">Due Date</div>
-                <div className="list_item_status_header">Status</div>
+                <div id="list_item_header">
+                    <div className="list_item_task_header">Task</div>
+                    <div className="list_item_due_date_header">Due Date</div>
+                    <div className="list_item_status_header">Status</div>
+                </div>
                 {
+                    // mapping the key and listItem for each Todo 
                     this.props.todoList.items.map((todoItem)=>(
                         <ListItemCard 
                             key={todoItem.key}
